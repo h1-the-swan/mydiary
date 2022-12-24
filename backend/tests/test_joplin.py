9610fead1437c9022531aad88208608d04d24fac
h1-the-swan <jason.portenoy@gmail.com>
2022-12-24 10:18:23 -0500

    add devcontainer.json files

--- backend/tests/test_joplin.py
+++ backend/tests/test_joplin.py
@@ -11,11 +11,12 @@ import pytest
 
 # load_dotenv(find_dotenv())
 
+JOPLIN_TEST_NOTEBOOK_ID = "84f655fb941440d78f993adc8bb731b3"
 
 @pytest.fixture(scope="session")
 def joplin_client():
     # with scope="session": the instance is shared across tests (so only initialized once)
-    with MyDiaryJoplin(init_config=False) as j:
+    with MyDiaryJoplin(init_config=False, notebook_id=JOPLIN_TEST_NOTEBOOK_ID) as j:
         yield j
 
 
@@ -34,10 +35,10 @@ def test_server_is_running(joplin_client: MyDiaryJoplin):
 
 
 def test_joplin_note_from_api(joplin_client: MyDiaryJoplin):
-    dt_str = "2022-01-18"
+    dt_str = "2022-01-14"
     dt = pendulum.parse(dt_str)
     note_id = joplin_client.get_note_id_by_date(dt)
-    assert note_id == "5d56d02b4a854eaf8e59650df3d0270e"
+    assert note_id == "d1f21d74ccc243388735a2c6779cd428"
     note = joplin_client.get_note(note_id)
     assert note.title == dt_str
     parent_id = note.parent_id
