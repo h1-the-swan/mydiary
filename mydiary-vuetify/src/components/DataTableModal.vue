0275026ae147408afc5aafb99ae2813d36c9941e
h1-the-swan <jason.portenoy@gmail.com>
2023-07-16 22:37:18 -0400

    minor edit

--- mydiary-vuetify/src/components/DataTableModal.vue
+++ mydiary-vuetify/src/components/DataTableModal.vue
@@ -1,7 +1,5 @@
 <template>
   <v-dialog v-model="dialog" max-width="500px">
-    <template v-slot:activator="{ props }">
-    </template>
     <v-card>
       <v-card-title>
         <span class="text-h5">{{ formTitle }}</span>
