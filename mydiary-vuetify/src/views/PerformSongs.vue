cd01060929d90b0f9597e5107e1613b99b26d2b9
h1-the-swan <jason.portenoy@gmail.com>
2023-08-08 20:04:00 -0400

    clean up working directory

--- mydiary-vuetify/src/views/PerformSongs.vue
+++ mydiary-vuetify/src/views/PerformSongs.vue
@@ -1,9 +1,9 @@
 <template>
-	<v-container>
-		<PerformSongsDropdown v-if="performSongsList" :items="performSongsList" />
-		<PerformSongsRandomButton v-if="performSongsList" :items="performSongsList" :current-id="Number(props.id)" />
-		<PerformSongCard :perform-song="performSong" :image-url="imageUrl" />
-	</v-container>
+  <v-container>
+    <PerformSongsDropdown v-if="performSongsList" :items="performSongsList" />
+    <PerformSongsRandomButton v-if="performSongsList" :items="performSongsList" :current-id="Number(props.id)" />
+    <PerformSongCard :perform-song="performSong" :image-url="imageUrl" />
+  </v-container>
 </template>
 
 <script lang="ts" setup>
@@ -16,22 +16,22 @@ Axios.defaults.baseURL = '/api';
 import { readPerformSong, PerformSongRead, getSpotifyImageUrl, readPerformSongsList } from '@/api';
 import { onMounted } from 'vue';
 const props = defineProps<{
-	id: number | string;
+  id: number | string;
 }>();
 const performSong = ref<PerformSongRead>();
 const imageUrl = ref('');
 const performSongsList = ref<PerformSongRead[]>();
 onMounted(async () => {
-	performSongsList.value = await readPerformSongsList({limit: 5000}).then((res) => res.data.sort((a, b) => a.name.localeCompare(b.name)));
+  performSongsList.value = await readPerformSongsList({ limit: 5000 }).then((res) => res.data.sort((a, b) => a.name.localeCompare(b.name)));
 })
 watchEffect(async () => {
-	imageUrl.value = '';
-	performSong.value = await readPerformSong(Number(props.id)).then((res) => res.data);
+  imageUrl.value = '';
+  performSong.value = await readPerformSong(Number(props.id)).then((res) => res.data);
 })
 watchEffect(async () => {
-	if (performSong.value && performSong.value.spotify_id) {
-		imageUrl.value = await getSpotifyImageUrl(performSong.value.spotify_id).then((res) => res.data);
-	}
+  if (performSong.value && performSong.value.spotify_id) {
+    imageUrl.value = await getSpotifyImageUrl(performSong.value.spotify_id).then((res) => res.data);
+  }
 })
 </script>
 
