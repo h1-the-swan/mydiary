40bd987098a521a25b1b530cc6fe8cee141d5c6f
h1-the-swan <jason.portenoy@gmail.com>
2023-08-29 13:30:52 -0400

    improve spotify save recent tracks button with snackbar

--- mydiary-vuetify/src/components/SpotifySaveRecentTracksButton.vue
+++ mydiary-vuetify/src/components/SpotifySaveRecentTracksButton.vue
@@ -1,15 +1,41 @@
 <template>
-	<v-btn @click="onClick">
-		Save recent Spotify tracks
-	</v-btn>
+  <v-btn @click="onClick" :loading="loading">
+    Save recent Spotify tracks
+  </v-btn>
+  <v-snackbar v-model="snackbar" timeout="3000">
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn color="pink" variant="text" @click="snackbar = false">
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script lang="ts" setup>
 import Axios from 'axios';
 Axios.defaults.baseURL = '/api';
 import { spotifySaveRecentTracksToDatabase } from '@/api';
+import { computed } from 'vue';
+import { ref } from 'vue';
+
+const loading = ref(false);
+const snackbar = ref(false);
+const numSaved = ref(0);
+const text = computed(() => {
+  return `${numSaved.value} tracks saved to database`;
+})
 
 async function onClick() {
-	await spotifySaveRecentTracksToDatabase().then((res) => res.data).then((res) => console.log(res));
+  loading.value = true;
+  await spotifySaveRecentTracksToDatabase()
+    .then((res) => res.data)
+    .then((res) => {
+      numSaved.value = res;
+      snackbar.value = true;
+      loading.value = false;
+
+    });
 }
 </script>
