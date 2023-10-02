621dbad3a087598955b50c0dfeb15ad99d360016
h1-the-swan <jason.portenoy@gmail.com>
2023-10-01 21:38:02 -0400

    check gcal auth in vuetify app

---
+++ mydiary-vuetify/src/components/GCalAuth.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn @click="onClick" :loading="loading">
+    Save recent Spotify tracks
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+import Axios from 'axios';
+Axios.defaults.baseURL = '/api';
+import { checkGCalAuth } from '@/api';
+import { ref } from 'vue';
+const loading = ref(false);
+async function onClick() {
+  loading.value = true;
+  await checkGCalAuth()
+    .then((res) => console.log(res))
+    .catch(() => console.log('auth credentials not working!'))
+}
+</script>
