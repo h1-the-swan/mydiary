db8d4ed7c19464d48cd2ec2ad0ac86f0509a9843
h1-the-swan <jason.portenoy@gmail.com>
2023-12-16 20:28:04 -0500

    clean up working tree laptop

---
+++ mydiary-vuetify/src/components/PerformSongsDataTable.vue
@@ -0,0 +1,14 @@
+<template>
+<v-data-table></v-data-table>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const headers = ref([
+  { title: 'id', key: 'id' },
+  { title: 'name', key: 'name' },
+  { title: 'artist_name', key: 'artist_name' },
+  { title: 'learned', key: 'learned' },
+])
+</script>
