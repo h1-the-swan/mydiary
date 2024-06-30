34088881a575232c40eb1ec45032a44dcbdf8727
h1-the-swan <jason.portenoy@gmail.com>
2024-06-30 14:13:53 -0400

    clean up working directory

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -25,33 +25,11 @@ root_logger = logging.getLogger()
 logger = root_logger.getChild(__name__)
 
 from mydiary.db import engine, Session, select, create_db_and_tables
-from mydiary.spotify_history import SpotifyTrackHistory, SpotifyTrackHistoryCreate
 
 
 def main(args):
-    spotify_datadir = Path(args.spotify_json_dir)
-
     # Create db and tables. This will skip tables that already exist.
     create_db_and_tables(engine)
-
-    tracks = []
-    for fp in spotify_datadir.glob("spotify_recent_tracks_dump*.json"):
-        data = json.loads(fp.read_text())
-        items = data["items"]
-        for item in items:
-            tracks.append(item)
-    with Session(engine) as session:
-        _unique = defaultdict(set)
-        i = 0
-        for t in tracks:
-            spotify_track = SpotifyTrackHistoryCreate.from_spotify_track(t)
-            if spotify_track.spotify_id not in _unique or spotify_track.played_at not in _unique[spotify_track.spotify_id]:
-                db_track = SpotifyTrackHistory.from_orm(spotify_track)
-                session.add(db_track)
-                _unique[spotify_track.spotify_id].add(spotify_track.played_at)
-                i += 1
-        logger.info(f"adding {i} tracks to spotifytrackhistory table")
-        session.commit()
 
 
 if __name__ == "__main__":
@@ -71,7 +49,6 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("spotify_json_dir", help="directory with spotify recent tracks dump json files")
     parser.add_argument("--debug", action="store_true", help="output debugging info")
     global args
     args = parser.parse_args()
