b2dd30a21de5db41fedb31dffcdd1e89f8f8ef6c
h1-the-swan <jason.portenoy@gmail.com>
2024-07-04 10:59:32 -0400

    test out image picker

--- mydiary-vuetify/src/components/NextcloudThumbnails.vue
+++ mydiary-vuetify/src/components/NextcloudThumbnails.vue
@@ -1,9 +1,23 @@
 <template>
-  <div v-if="imgUrls">
-    <p v-for="(url) in imgUrls" :key="url">
-      {{ url }}
-      <v-img :src="`/api/nextcloud/thumbnail_img?url=${url}`"></v-img>
-    </p>
+  <div v-if="nextCloudThumbs">
+    <v-form @submit.prevent="onSubmit">
+      <v-row>
+        <v-col
+          v-for="(nImage) in nextCloudThumbs"
+          :key="nImage.url"
+          class="d-flex child-flex nextcloud-thumb"
+          :class="{selectedImg: nImage.selected}"
+          cols="4"
+        >
+        <v-img
+          :src="nImage.src"
+          aspect-ratio="1"
+          @click="nImage.selected = !nImage.selected"
+        ></v-img>
+        </v-col>
+      </v-row>
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
   </div>
 </template>
 
@@ -11,14 +25,46 @@
 import Axios from 'axios'
 Axios.defaults.baseURL = '/api'
 import { nextcloudPhotosThumbnailUrls } from '@/api'
-import { ref, watch } from 'vue';
+import { ref, watch, watchEffect } from 'vue';
 const props = defineProps<{
   dt: string;
 }>()
-const imgUrls = ref<string[]>()
+interface INextCloudThumb {
+  url: string;
+  src: string;
+  selected: boolean;
+}
+const nextCloudThumbs = ref<INextCloudThumb[]>([])
 async function fetchImageUrls() {
-  imgUrls.value = undefined
-  imgUrls.value = (await nextcloudPhotosThumbnailUrls(props.dt)).data
+  const imgUrls = (await nextcloudPhotosThumbnailUrls(props.dt)).data
+  nextCloudThumbs.value = imgUrls.map((url => {
+    return {
+      url: url,
+      src: `/api/nextcloud/thumbnail_img?url=${url}`,
+      selected: false
+    }
+  }))
 }
 watch(props, fetchImageUrls, { immediate: true })
+watchEffect(() => {
+  const numSelected = nextCloudThumbs.value.filter((t => t.selected)).length
+  console.log(`${numSelected} selected!`)
+  console.log(nextCloudThumbs.value)
+})
+
+function onSubmit() {
+  const numSelected = nextCloudThumbs.value.filter((t => t.selected)).length
+  console.log(`submitted! ${numSelected} selected!`)
+
+}
 </script>
+
+<style>
+.nextcloud-thumb {
+  opacity: 50%;
+}
+
+.nextcloud-thumb.selectedImg {
+  opacity: 100%;
+}
+</style>
