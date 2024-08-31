d0f0a327edcaa93e9caebfa368bbf19a243cc7f8
h1-the-swan <jason.portenoy@gmail.com>
2024-08-31 09:10:15 +0200

    fully delete react app frontend (in favor of vue app)

--- frontend/src/routes/performSongs/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./performSongs";
-
-export * from "./performSongAddOrCreate";
--- frontend/src/routes/recipes.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Typography } from "antd";
-
-const Recipes = () => {
-  return <Typography.Title>Recipes</Typography.Title>;
-};
-
-export default Recipes;
-
